688896ab10fd6aecbcce699c919e7d6a46309ddf
dennis84 <d.dietrich84@gmail.com>
2014-05-31 07:29:28 +0200

    fix status view

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -16730,15 +16730,11 @@ module.exports = function(conn) {
   var Status = ripple('<div class="status"></div>')
 
   Status.ready(function() {
-    conn.once('open', _.bind(this.open, this))
+    this.el.innerHTML = templateOpen
     conn.once('opened', _.bind(this.opened, this))
     conn.once('closed', _.bind(this.closed, this))
     conn.once('code', _.bind(this.destroy, this))
   })
-
-  Status.prototype.open = function(conn) {
-    this.el.innerHTML = templateOpen
-  }
 
   Status.prototype.opened = function(conn) {
     this.el.innerHTML = templateOpened
